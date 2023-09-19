e070f2b31e8fa879a5b50b0675c357a727dd317f
Surya Poojary <77871514+Gh0st-sniper@users.noreply.github.com>
2023-09-19 14:32:03 +0530

    added home view

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home.home');
 });
 
 Route::resource('users', UserController::class);
