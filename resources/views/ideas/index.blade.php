5e82b6ca530ad67e50be88d53b630927f73cdb36
Surya Poojary <77871514+Gh0st-sniper@users.noreply.github.com>
2023-09-19 20:58:18 +0530

    created index.blade.php in ideas

---
+++ resources/views/ideas/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> ideas </title>
+</head>
+<body>
+
+<h1> Create an Idea </h1>
+
+<form action="/ideas" method="post">
+
+<label for=""> Title </label>
+<input type="text" name="title" id=""><br>
+<label for=""> Description </label>
+<input type="text" name="title" id=""><br>
+<input type="submit" value="Create Idea">
+
+
+</form>
+    
+</body>
+</html>
