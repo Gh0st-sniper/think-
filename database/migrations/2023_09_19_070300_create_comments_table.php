f060b6edcce31594d07be684d18725ecc2ed3aae
Surya Poojary <77871514+Gh0st-sniper@users.noreply.github.com>
2023-09-21 08:57:01 +0530

    added text field

--- database/migrations/2023_09_19_070300_create_comments_table.php
+++ database/migrations/2023_09_19_070300_create_comments_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('commentbody');
             $table->timestamps();
             $table->foreignId('idea_id');
         });
